b3b2754eb1be82b0fb24f32ff3a3df6d0006be11
Made Satria Wibawa <mdsatria@gmail.com>
2020-08-28 22:50:35 +0800

    create new label

--- dataLoad.py
+++ dataLoad.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import torch
-from sklearn import preprocessing
 
 from PIL import Image
 from torch.utils.data import Dataset
@@ -11,10 +10,6 @@ class AksaraBali(Dataset):
         self.annotations = pd.read_csv(csv_file, header=None)
         self.root_dir = root_dir
         self.transform = transform
-        labels = self.annotations[1]
-        le = preprocessing.LabelEncoder()
-        targets = le.fit_transform(labels.astype(str))
-        self.label = (targets)
        
     def __len__(self):
         return len(self.annotations)
@@ -22,7 +17,7 @@ class AksaraBali(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.root_dir, self.annotations.iloc[index,0])
         image = Image.open(img_path).resize((224, 224))
-        y_label = torch.tensor(self.label[index], dtype=torch.long)
+        y_label = torch.tensor(self.annotations.iloc[index,2], dtype=torch.long)
         
         if self.transform:
             image = self.transform(image)
