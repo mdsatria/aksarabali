bb4a09924b7931be559b45b3d266967fa9b8c77f
Made Satria Wibawa <mdsatria@gmail.com>
2020-09-05 11:47:35 +0800

    fix path, cleaning data directory

--- Transfer Learning/modelTrain.py
+++ Transfer Learning/modelTrain.py
@@ -1,14 +1,35 @@
+import pandas as pd 
+import numpy as np
+import os
 import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
-from dataLoad import AksaraBali
+from torch.utils.data import DataLoader, Dataset
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+class AksaraBali(Dataset):
+    def __init__(self, csv_file, root_dir, height=224, width=224, transform=None):
+        self.annotations = pd.read_csv(csv_file, header=None)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.height = height
+        self.width = width
+       
+    def __len__(self):
+        return len(self.annotations)
+    
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index,0])
+        image = Image.open(img_path).resize((self.height, self.width))
+        y_label = torch.tensor(self.annotations.iloc[index,2], dtype=torch.long)
+        
+        if self.transform:
+            image = self.transform(image)
+            
+        return(image, y_label)
+    
 def check_accuracy(loader, model, acc_type):
     num_correct = 0
     num_samples = 0
@@ -27,7 +48,9 @@ def check_accuracy(loader, model, acc_type):
         print(f'{acc_type} got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100:.2f}') 
     
     model.train()
-    
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 in_channel = 3 
 num_classes = 133
 learning_rate = 0.001
@@ -38,12 +61,12 @@ num_epoch = 1
 my_transform = transforms.Compose([transforms.ToTensor(),
                                    transforms.Normalize(mean=(0.5005, 0.4670, 0.4174), std=(0.0813, 0.0534, 0.0932))])
 
-train_set = AksaraBali(csv_file="label/train_label.csv",
-                       root_dir="images/train",
+train_set = AksaraBali(csv_file="../data/Train Augmix/train_label.csv",
+                       root_dir="../data/Train Augmix/",
                        transform=my_transform)
 
-test_set = AksaraBali(csv_file="label/test_label.csv",
-                       root_dir="images/test",
+test_set = AksaraBali(csv_file="../data/Test/test_label.csv",
+                       root_dir="../data/Test/",
                        transform=my_transform)
 
 #train_set = torch.utils.data(dataset)
